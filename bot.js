a3f48d246f1bf183874698befb5083ae31753dd2
Gyan Gaurav <gyan.gaurav777@gmail.com>
2018-04-27 14:13:01 +0530

    posting images on twitter

--- bot.js
+++ bot.js
@@ -1,53 +1,96 @@
+// console.log('The image bot is starting');
+
+// const Twit = require('twit');
+
+// const config = require('./config');
+
+// var T = new Twit(config);
+
+// const exec = require('child_process').exec;
+// const fs = require('fs');
+
+// tweetIt();
+// // setInterval(tweetIt, 1000*20);
+
+// function tweetIt() {
+//     const cmd = 'processing-java --sketch=`pwd`/rainbow --run';
+//     exec(cmd, processing);
+  
+//   function processing() {
+//     const filename = 'rainbow/output.png';
+//     const params = {
+//       encoding: 'base64'
+//   	}
+//       const b64 = fs.readFileSync(filename, params);
+
+//     T.post('media/upload', { media_data: b64 }, uploaded);
+
+//     function uploaded(err, data, response) {
+// 		const id = data.media_id_string;
+//         const tweet = {
+// 	    status: '#codingrainbow live from node.js',
+// 	    media_ids: [id]
+// 	  }
+//       T.post('statuses/update', tweet, tweeted);
+
+//     }
+
+// 	function tweeted(err, data, response) {
+// 	  if (err) {
+// 	  	console.log("Something went wwrong!");
+// 	  } else {
+// 	    console.log("It worked!");
+// 	  }
+// 	}
+//   }
+// }
+
+
+
+
+
+
+
 console.log('!!!!!!!!!!  WORKING   !!!!!!!!!!!!!!!');
 
 const Twit = require('twit');
 
 const config = require('./config');
+
 const T = new Twit(config);
-const thought = [ "Be happy for this moment. This moment is your life.",
-                  "Because of your smile, you make life more beautiful." ,
-                  "Lighten up, just enjoy life, smile more, laugh more, and don't get so worked up about things." 
-                ]
 
-const stream = T.stream('user');
-stream.on('follow', followed);
+const exec = require('child_process').exec;
+const fs = require('fs');
 
-function followed(eventMsg){
-    console.log('posting back to follower');
-    const name = event.source.name;
-    const screenName = event.source.screen_name;
-    tweetIt('.@' + screenName + 'Hello!!!!');
-}
+ tweetIt();
+// setInterval(tweetIt, 1000* 2)
 
-function tweetIt ( txt ) {
-    let r = Math.floor(Math.random() * 100) ;
-    
-    let tweet = {
-        status : txt      //'new random number post : #' + r //thought[i]           // write post content
-    };
-    console.log(tweet);
-    // T.post('statuses/update', tweet, tweeted);
-    function tweeted( err, data, response) {
-    if (err) {
-        console.log("something went wrong!");
-    } else {
-        console.log("****it worked****");
+function tweetIt () {
+    processing();
+    function processing(){
+        const filename = "rainbow/output.png";
+        const param = {
+            encoding : 'base64'
+        }
+        const b64 = fs.readFileSync( filename, param );
+        
+        T.post( 'media/upload' , { media_data: b64 }, uploaded);
+
+        function uploaded(err, data, respose) {
+            var id = data.media_id_string;
+            var tweet = {
+                status: 'media post', 
+                media_ids: [id] 
+            }
+            T.post('statuses/update', tweet, tweeted);
+        }
+
+        function tweeted( err, data, response) {
+            if (err) {
+                console.log("something went wrong!");
+            } else {
+                console.log("****it worked****");
+            }
+        }
     }
-    }
-};     
-
-
-//** Search 2 posts having word 'banana'.
-// const param = { 
-//     q: 'banana',   // search term
-//     count: 2 }
-
-// T.get('search/tweets',param ,gotData);
- 
-// function gotData(err, data, response) { 
-//     const tweets = data.statuses;
-//    for (let i=0; i < tweets.length ; i++ ){
- //       console.log( tweets[i].text );
-   // }
-    
-  
+};
